181ae54e2857e505cd6a59d8ae44c2d5514ffbcc
Mat_L <txalka@mailfence.com>
2020-11-08 02:31:38 -0300

    CHG: se agrega metodo para cifrar mensaje ingresado en Security_act.java

--- app/src/main/java/com/w/evaluacion2_mateo_lopez/Info_act.java
+++ app/src/main/java/com/w/evaluacion2_mateo_lopez/Info_act.java
@@ -2,13 +2,35 @@ package com.w.evaluacion2_mateo_lopez;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 public class Info_act extends AppCompatActivity {
+
+    private TextView textView;
+    private VideoView video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_act);
+
+        textView = (TextView)findViewById(R.id.textView);
+        video = (VideoView)findViewById(R.id.video);
+
+        video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.n));
+        MediaController mc = new MediaController(this);
+        video.setMediaController(mc);
+        video.start();
+    }
+
+    public void Maps(View v) {
+        Intent i = new Intent(this, Maps_act.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/w/evaluacion2_mateo_lopez/Security_act.java
+++ app/src/main/java/com/w/evaluacion2_mateo_lopez/Security_act.java
@@ -3,12 +3,58 @@ package com.w.evaluacion2_mateo_lopez;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Security_act extends AppCompatActivity {
+
+    private EditText mensaje;
+    private Button btnCifrar;
+    private TextView cifrado;
+
+    private static final String AES = "AES";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_security_act);
+
+        mensaje = (EditText) findViewById(R.id.edtPassword);
+        btnCifrar = (Button) findViewById(R.id.btnCifrar);
+        cifrado = (TextView) findViewById(R.id.textViewCifrado);
+
+        btnCifrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    cifrar();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void cifrar() throws Exception {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
+            keyGenerator.init(128);
+            SecretKey secretKey = keyGenerator.generateKey();
+            byte[] bytes = secretKey.getEncoded();
+            SecretKeySpec secretKeySpec = new SecretKeySpec(bytes, AES);
+            Cipher cipher = Cipher.getInstance(AES);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+            String mmensaje = mensaje.getText().toString();
+            byte[] ccifrado = cipher.doFinal(mmensaje.getBytes());
+            cifrado.setText(ccifrado.toString());
     }
 }
